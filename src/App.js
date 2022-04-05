c903c6f30a9dcd07d82a10206b61b6bb1ba9baac
Weston Isheim <wisheim@gmail.com>
2022-04-05 16:26:27 -0600

    Added incrementer/decrementor and useEffect displaying JSON on page load

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import './App.css';
 import Axios from 'axios'
 
 
 function App() {
+//API practice
   const [ pokemon, setPokeman ] = useState("")
   const [ pokemonName, setPokemonName ] = useState("")
 
@@ -20,12 +21,52 @@ function App() {
     )
     }
 
+//Incrament/decrament counter practice
+let [ counter, setCounter ] = useState(0)
+
+//API part2 Practice
+
+const [ data, setData ] = useState('')
+const [ userInfo, setUserInfo ] = useState([])
+
+useEffect(() => {
+  getData()
+
+}, [])
+
+const getData = () => {
+  Axios.get(`https://pokeapi.co/api/v2/pokemon/pikachu/`)
+    .then((response) => {
+      setData(JSON.stringify(response))
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
+
   return (
     <div>
+    {/* API practice */}
      <h1>hello</h1>
      <input placeholder="pokemon" onChange={typePokemonName}></input>
      <button onClick={getPokemon}>Get Pokemon</button>
      Name: {pokemon}
+
+     {/* Increment/decrement Counter Practice  */}
+     <div>
+       <button onClick={() => {
+         setCounter(counter -=1)
+       }}>-</button>
+       {counter}
+       <button onClick={() => {
+         setCounter(counter +=1)
+       }}>+</button>
+     </div>
+
+    {/* API part2 Practice */}
+    <button onClick={getData}>get data</button>
+    {data}
+
     </div>
   );
 }
