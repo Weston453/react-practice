b7834ea66b7d9b7de566118a7a53cc1ffa9e7ad5
Weston Isheim <wisheim@gmail.com>
2022-04-05 15:34:51 -0600

    Axios api get request practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
+import Axios from 'axios'
+
 
 function App() {
+  const [ pokemon, setPokeman ] = useState("")
+  const [ pokemonName, setPokemonName ] = useState("")
+
+  const typePokemonName = (e) => {
+    setPokemonName(e.target.value)
+  }
+  
+  const getPokemon = () => {
+    Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}/`).then(
+      (response) => {
+        setPokeman(JSON.stringify((response)['data']['stats']))
+        console.log((response)['data'])
+      }
+    )
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <h1>hello</h1>
+     <input placeholder="pokemon" onChange={typePokemonName}></input>
+     <button onClick={getPokemon}>Get Pokemon</button>
+     Name: {pokemon}
     </div>
   );
 }
